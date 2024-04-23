1a7310c40f004404d37e3a9b2747ed0261eb66d1
Maxwell Aussem <aussem.maxwell@gmail.com>
2024-04-23 12:45:35 -0400

    first push of page

---
+++ pages/SubscribeForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+import React, { useState } from 'react';
+import { Button, TextField, Snackbar, Grid, IconButton } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+export default function SubscribeForm() {
+  const [email, setEmail] = useState('');
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    setIsClicked(true); // Trigger animation
+        setTimeout(() => setIsClicked(false), 300);
+    if (!email) {
+      setError('Email is required');
+      return;
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      setError('Entered value does not match email format');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/subscribe', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        setOpen(true);
+        setEmail('');
+        setError('');
+      } else {
+        setError('Failed to subscribe');
+      }
+    } catch (error) {
+      setError('Failed to subscribe');
+      console.error('Failed to subscribe:', error);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{ maxWidth: '320px', margin: 'auto' }}>
+      <img 
+        src="/logo.png" 
+        alt="Company Logo" 
+        style={{ width: 320, height: 320, margin: 'auto', marginTop: -170, display: 'block' }} 
+      />
+      <form onSubmit={handleSubmit} noValidate >
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Become a Founding Member"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={!!error}
+          helperText={error}
+        />
+        <Button 
+          type="submit" 
+          fullWidth 
+          variant="contained" 
+          style={{
+            backgroundColor: 'rgb(84, 41, 201)', 
+            color: 'white',
+            transform: isClicked ? 'scale(0.95)' : 'scale(1)',
+            transition: 'transform 300ms ease-in-out'
+        }}
+        >
+          Subscribe
+        </Button>
+
+
+      </form>
+      <Grid container justifyContent="space-evenly" style={{ marginTop: 20  }}>
+        <IconButton aria-label="facebook" onClick={() => window.location.href = 'https://facebook.com'} style={{
+            backgroundColor: 'rgb(84, 41, 201)', 
+            color: 'white',
+            transform: isClicked ? 'scale(0.95)' : 'scale(1)',
+            transition: 'transform 300ms ease-in-out'
+        }}>
+          <FacebookIcon />
+        </IconButton>
+        <IconButton aria-label="twitter" onClick={() => window.location.href = 'https://twitter.com'} style={{
+            backgroundColor: 'rgb(84, 41, 201)', 
+            color: 'white',
+            transform: isClicked ? 'scale(0.95)' : 'scale(1)',
+            transition: 'transform 300ms ease-in-out'
+        }}>
+          <TwitterIcon />
+        </IconButton>
+        <IconButton aria-label="instagram" onClick={() => window.location.href = 'https://instagram.com'} style={{
+            backgroundColor: 'rgb(84, 41, 201)', 
+            color: 'white',
+            transform: isClicked ? 'scale(0.95)' : 'scale(1)',
+            transition: 'transform 300ms ease-in-out'
+        }}>
+          <InstagramIcon />
+        </IconButton>
+      </Grid>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} message="Thank you for subscribing!" />
+    </div>
+  );
+}
+
---
+++ pages/api/subscribe.ts
@@ -0,0 +1,65 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import mailchimp from '@mailchimp/mailchimp_marketing';
+
+const apiKey = process.env.MAILCHIMP_API_KEY;
+
+mailchimp.setConfig({
+  apiKey: apiKey,
+  server: `us7`  // Correct server address
+});
+
+interface SubscribeRequest {
+  email: string;
+}
+
+interface ApiResponse {
+  error: string | null;
+  message?: string;
+}
+
+interface MailchimpError {
+  response: {
+      body: {
+          detail: string;
+      };
+  };
+  message: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse>
+): Promise<void> {
+  if (req.method !== 'POST') {
+      res.status(405).json({ error: 'Method Not Allowed' });
+      return;
+  }
+
+  const { email } = req.body as SubscribeRequest;
+
+  if (!email) {
+      res.status(400).json({ error: 'Email is required' });
+      return;
+  }
+
+  try {
+    const listId = "0313cdf088";
+    const response = await mailchimp.lists.addListMember(listId, {
+        email_address: email,
+        status: 'subscribed',
+    });
+
+    console.log('Mailchimp response:', response);
+    res.status(200).json({ error: null, message: 'Subscription successful' });
+  } catch (error: unknown) {
+    console.error('Subscription error:', error);
+    let errorMessage = 'An unexpected error occurred';
+
+    if (typeof error === "object" && error !== null) {
+        const mailchimpError = error as MailchimpError;
+        errorMessage = mailchimpError.response?.body?.detail || mailchimpError.message || errorMessage;
+    }
+    
+    res.status(500).json({ error: errorMessage });
+  }
+}
